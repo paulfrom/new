30a2e4c6ce72ddabcaac2f6f798c1ec09f686287
paulfrom <lslin6@gmail.com>
2013-12-01 11:58:20 +0800

    hhhhh

--- web/Lib/Action/CommonAction.class.php
+++ web/Lib/Action/CommonAction.class.php
@@ -7,17 +7,15 @@
    			$this->_filter($map);
    		}
    		$this->_list();
-   		// $this->assign("name",$name);
-   		//echo "xxx";
    		$this->display();
    		return;
    	}
 
    	/*
-   	 * map 鏂规硶
+   	 * map 閺傝纭�
    	*/
    	protected function _search($name = '') {
-   		//鐢熸垚鏌ヨ鏉′欢
+   		//閻㈢喐鍨氶弻銉嚄閺夆�娆�
    		if (empty($name)) {
    			$name = "article";//$this->getActionName();
    		}
@@ -33,15 +31,15 @@
    	}
    	/**
    	 +----------------------------------------------------------
-   	 * 鏍规嵁琛ㄥ崟鐢熸垚鏌ヨ鏉′欢
-   	 * 杩涜鍒楄〃杩囨护
+   	 * 閺嶈宓佺悰銊ュ礋閻㈢喐鍨氶弻銉嚄閺夆�娆�
+   	 * 鏉╂稖顢戦崚妤勩�鏉╁洦鎶�
    	 +----------------------------------------------------------
    	 * @access protected
    	 +----------------------------------------------------------
-   	 * @param Model $model 鏁版嵁瀵硅薄
-   	 * @param HashMap $map 杩囨护鏉′欢
-   	 * @param string $sortBy 鎺掑簭
-   	 * @param boolean $asc 鏄惁姝ｅ簭
+   	 * @param Model $model 閺佺増宓佺�纭呰杽
+   	 * @param HashMap $map 鏉╁洦鎶ら弶鈥叉
+   	 * @param string $sortBy 閹烘帒绨�
+   	 * @param boolean $asc 閺勵垰鎯佸锝呯碍
    	 +----------------------------------------------------------
    	 * @return void
    	 +----------------------------------------------------------
@@ -49,16 +47,16 @@
    	 +----------------------------------------------------------
    	 */
    	protected function _list() {
-   		//     	//鎺掑簭瀛楁 榛樿涓轰富閿悕
+   		//     	//閹烘帒绨�妤侇唽 姒涙顓绘稉杞板瘜闁款喖鎮�
    		$name ="article"; //$this->getActionName();
    		$model = D($name);
    	    $list = $model->order('Id ASC')->limit(0,20)->select();   			 
    		//$this->assign('page', $page);
    		$this->assign("list",$list);
-   			//     		$sortImg = $sort; //鎺掑簭鍥炬爣
-   			//     		$sortAlt = $sort == 'desc' ? '鍗囧簭鎺掑垪' : '鍊掑簭鎺掑垪'; //鎺掑簭鎻愮ず
-   			//     		$sort = $sort == 'desc' ? 1 : 0; //鎺掑簭鏂瑰紡
-   			//     		//妯℃澘璧嬪�鏄剧ず
+   			//     		$sortImg = $sort; //閹烘帒绨崶鐐垼
+   			//     		$sortAlt = $sort == 'desc' ? '閸楀洤绨幒鎺戝灙' : '閸婃帒绨幒鎺戝灙'; //閹烘帒绨幓鎰仛
+   			//     		$sort = $sort == 'desc' ? 1 : 0; //閹烘帒绨弬鐟扮础
+   			//     		//濡剝婢樼挧瀣拷閺勫墽銇�
    			//     		$this->assign('list', $voList);
    			//     		$this->assign('sort', $sort);
    			//     		$this->assign('order', $order);
